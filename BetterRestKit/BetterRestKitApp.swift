f903424424dff0f8e08b9eb125283c459feb73fa
Kitwana Akil <kakil@me.com>
2023-07-13 20:19:48 -0400

    Initial Commit

---
+++ BetterRestKit/BetterRestKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestKitApp.swift
+//  BetterRestKit
+//
+//  Created by Kitwana Akil on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
